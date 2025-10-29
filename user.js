f141ada4e25c59e014ddb1e0600b8b08cdf47dc3
Sarabpreet1023 <161135692+Sarabpreet1023@users.noreply.github.com>
2025-10-29 14:25:31 +0530

    Add user data module with sample users

---
+++ user.js
@@ -0,0 +1,6 @@
+// users.js
+module.exports = [
+  { id: 1, username: 'admin', password: 'admin123', role: 'Admin' },
+  { id: 2, username: 'mod', password: 'mod123', role: 'Moderator' },
+  { id: 3, username: 'user', password: 'user123', role: 'User' }
+];
